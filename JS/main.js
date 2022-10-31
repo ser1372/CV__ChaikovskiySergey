ceb471174f3d34b610382b8b1a2a75c8208f3f1b
ser1372 <ser1372@ukr.net>
2022-10-31 16:25:59 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,61 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    /*catch general sections*/
+    const
+        d = document,
+        main = d.querySelector('main'),
+        aside = d.querySelector('aside'),
+        header = d.querySelector('header'),
+        footer = d.querySelector('footer');
+
+    /*Header*/
+    header.querySelector('.head-name')
+        .insertAdjacentHTML("afterbegin", `${data.name.firstName} ${data.name.lastName}`)
+    header.querySelector('.g-info')
+        .insertAdjacentHTML('beforeend', `<h4>${data.position.map(item => `<span>${item}</span>`).join('•')}</h4>`);
+
+    /*Aside*/
+    /*make header padding*/
+    aside.style.paddingTop = main.style.paddingTop = `${header.offsetHeight / 16}rem`;
+
+    aside.querySelector('.photo')
+        .insertAdjacentHTML('afterbegin', `<img src="${data.photoPath}" alt="myPhoto">`);
+    aside.querySelector('.lang_section')
+        .insertAdjacentHTML('beforeend', `<div class="lang-tags">${data.makeLanguages()}</div>`)
+    aside.querySelector('.skill-tags')
+        .insertAdjacentHTML('afterbegin', `${data.makeSkills()}`);
+    aside.querySelector('.hobby-tags')
+        .insertAdjacentHTML('beforeend', `${data.makeInterests()}`);
+    aside.querySelector('.contact-items')
+        .insertAdjacentHTML('afterbegin', `${data.makeContacts()}`);
+
+    /*Main*/
+    main.querySelector('.myStory')
+        .insertAdjacentHTML('beforeend', `${data.makeMyStory()}`);
+    main.querySelector('.experience')
+        .insertAdjacentHTML('beforeend', `${data.makeWorkExperience()}`);
+    main.querySelector('.education')
+        .insertAdjacentHTML('beforeend', `${data.makeEducation()}`);
+
+    /*Footer*/
+    footer.querySelector('.socials')
+        .insertAdjacentHTML('afterbegin', `${data.makeSocialLinks()}`);
+});
